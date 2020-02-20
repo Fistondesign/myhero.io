b4147354a65021e34fac3063ade8de4347082134
knfizzo <knfintaganda@gmail.com>
2020-02-20 11:04:48 +0200

    Updates

---
+++ precache-manifest.05c5b6012e7e1aff7611f03afbf7df5b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd42936c93e3bce2003e8683758a9063",
+    "url": "/myhero.io/index.html"
+  },
+  {
+    "revision": "aca43bdfcac7a134a777",
+    "url": "/myhero.io/static/css/main.8ac5c7b5.chunk.css"
+  },
+  {
+    "revision": "dc3a5819fec25def11b1",
+    "url": "/myhero.io/static/js/2.3b28eb55.chunk.js"
+  },
+  {
+    "revision": "52dfbeda0d8fcd66821ea0be571eb11e",
+    "url": "/myhero.io/static/js/2.3b28eb55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca43bdfcac7a134a777",
+    "url": "/myhero.io/static/js/main.7c5cb0fc.chunk.js"
+  },
+  {
+    "revision": "4f467b453d2f4948d915",
+    "url": "/myhero.io/static/js/runtime-main.4b439dad.js"
+  },
+  {
+    "revision": "3f4b22a5f80a10a92526dc2a75c7b26b",
+    "url": "/myhero.io/static/media/headerprofile.3f4b22a5.png"
+  },
+  {
+    "revision": "82f6e9f2ec5cc321b83a14e4c0f40a51",
+    "url": "/myhero.io/static/media/kag.82f6e9f2.jpg"
+  },
+  {
+    "revision": "d2f2b8ae7d7765b7f1d977b97c17a993",
+    "url": "/myhero.io/static/media/kagame.d2f2b8ae.png"
+  },
+  {
+    "revision": "f1bcf6cb24d4328d250132589d74a879",
+    "url": "/myhero.io/static/media/paul.f1bcf6cb.png"
+  },
+  {
+    "revision": "f8caa051495d1a26595b384ce0fd0d7e",
+    "url": "/myhero.io/static/media/paul1.f8caa051.png"
+  }
+]);
